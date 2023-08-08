5f1959899ca80f077ee3912b42d0b4df77d4a7c6
riazmannan01 <riaz.mannan01@gmail.com>
2023-08-08 14:48:47 +0100

    247 riaz was here

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Riaz"));
+        System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayGoodbye());
     }
 
